088db5386686ecc3943434d626f54da2d9b1c1ec
victoriapenas <v.penas@ittravelservices.com>
2020-10-28 11:56:54 +0100

    subo forms

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     #post/ significa que la URL debería empezar con la palabra post seguida por una /.
     #<int:pk> significa que Django buscará un número entero y se lo pasará a la vista en una variable llamada pk.
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404 , get_list_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 """
 El punto antes de models indica el directorio actual o la aplicación actual.
@@ -18,7 +20,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #posts = get_list_or_404( Post, published_date__lte=timezone.now()) #en teoría con esta función si se recupera una lista vacía de posts, muestra la pag. 404
     args = {} #declaro diccionario
-    args['posts'] = posts #creo y añado una key  al diccionario
+    args['posts'] = posts #creo y añado una key al diccionario
     return render(request, 'blog/post_list.html', args) #envío el diccionario como argumentos
 
 def post_detail(request, pk):
@@ -28,3 +30,29 @@ def post_detail(request, pk):
     args = {'post': post, 'posts':posts}
     return render(request, 'blog/post_detail.html', args)
     
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
